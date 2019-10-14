884902f12a375dd00d920263642e53b50a1c65fb
granttaylor448 <granttaylor448@gmail.com>
2019-10-14 22:36:37 +0000

    changed to include string literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😃😃😃😃 Assertion Passed: " + actual + " === " + expected);
+    console.log(`😃😃😃😃 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("😡😡😡😡Assertion Failed: " + actual + " !== " + expected);
+    console.log(`😡😡😡😡Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
