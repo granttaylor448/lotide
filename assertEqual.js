775c9125fabc0baac97c9b2648ed35f072091967
granttaylor448 <granttaylor448@gmail.com>
2019-10-14 22:21:43 +0000

    created a js function to imitate console.assert

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😃😃😃😃 Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("😡😡😡😡Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("thissentencehasnospaces", "thissentencehasnospaces");
+assertEqual("apple", "banana");
+assertEqual(87, 88);
+assertEqual(666, 666);
