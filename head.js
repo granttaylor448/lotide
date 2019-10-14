c93518e3d4b2bbdd7fd253fb5b956acab455d81b
granttaylor448 <granttaylor448@gmail.com>
2019-10-14 23:31:14 +0000

    forgot to lint

--- head.js
+++ head.js
@@ -7,10 +7,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function(actual){
-    return actual[0]
-  }
+const head = function(actual) {
+  return actual[0];
+};
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1)
-assertEqual(head([]), 5)
+assertEqual(head([1]), 1);
+assertEqual(head([]), 5);
