7182813e3fda157ffd7d2b73d2e2eb7bc919b13c
granttaylor448 <granttaylor448@gmail.com>
2019-10-15 22:57:05 +0000

    completed assignment function now compares arrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+let match = false
+const eqArrays = function (actual, expected){
+  if (actual[0] === expected [0] && actual[1] === expected [1] && actual[2] === expected [2]) {
+    match = true
+  } else {
+    match = false
+  }  return match
+}
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😡😡😡😡Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false )
