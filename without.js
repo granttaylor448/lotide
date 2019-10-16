9900d0e65686b269193ece49bc26300f2766072a
granttaylor448 <granttaylor448@gmail.com>
2019-10-16 15:18:57 +0000

    minor change eliminating bug in the code

--- without.js
+++ without.js
@@ -34,7 +34,7 @@ return newArray
 assertArrayEqual(without([1, 2, 3], [1]), [2, 3] )
 assertArrayEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"] )
 
-// const words = ["hello", "world", "lighthouse"];
-// without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+//Make sure the original array was not altered by the without function
+assertArrayEqual(words, ["hello", "world", "lighthouse"]);
